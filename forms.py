81f36dc53840b260c02c4302362d66cf10d73edf
Ian Herdegen <ianherdegen@Ians-Macbook-Pro.local>
2020-11-11 16:56:30 -0500

    Initial COMMIT

---
+++ forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from boats.models import Type
+
+
+# Create the form class.
+class TypeForm(ModelForm):
+    class Meta:
+        model = Type
+        fields = '__all__'
---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+# https://docs.djangoproject.com/en/3.0/topics/http/urls/
+app_name = 'boats'
+urlpatterns = [
+    path('', views.BoatList.as_view(), name='all'),
+    path('main/create/', views.BoatCreate.as_view(), name='boat_create'),
+    path('main/<int:pk>/update/', views.BoatUpdate.as_view(), name='boat_update'),
+    path('main/<int:pk>/delete/', views.BoatDelete.as_view(), name='boat_delete'),
+    path('lookup/', views.TypeView.as_view(), name='type_list'),
+    path('lookup/create/', views.TypeCreate.as_view(), name='type_create'),
+    path('lookup/<int:pk>/update/', views.TypeUpdate.as_view(), name='type_update'),
+    path('lookup/<int:pk>/delete/', views.TypeDelete.as_view(), name='type_delete'),
+]
+
+# Note that type_ and boat_ give us uniqueness within this appliboation
